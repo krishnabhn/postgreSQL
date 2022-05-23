909e0edd431b0e1bc4b9de7e0f61fadb07c501ff
krishnabhn <105424593+krishnabhn@users.noreply.github.com>
2022-05-23 15:48:09 +0530

    Update var.tf

--- var.tf
+++ var.tf
@@ -6,17 +6,17 @@ variable "vpc_cidr" {
 # Defining azs for subnets
 variable "azs" {
     type= list(string)
-    default =["us-east-2a", "us-east-2b", "us-east-2a","us-east-2b"]
+    default =["us-east-1a", "us-east-1b", "us-east-2a","us-east-2b"]
 }
 # Defining CIDR Block for public Subnets
-variable "pubsub" {
-    type= list(string)
-    default =["192.168.0.0/24","192.168.1.0/24"]
+#variable "pubsub" {
+#    type= list(string)
+  #  default =["192.168.0.0/24","192.168.1.0/24"]
 }
 
 variable "pvtsub" {
     type= list(string)
-    default =["192.168.2.0/24", "192.168.3.0/24"]
+    default =["192.168.0.0/24", "192.168.1.0/24","192.168.2.0/24","192.168.3.0/24"]
 }
 
 variable "demosubnet" {
